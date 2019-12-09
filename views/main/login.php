d77469e7ea55b3d73888cc48ab84d3b6e298befa
vagrant <vagrant@homestead>
2019-12-09 10:24:10 +0000

    add styles for login exceptions

--- views/main/login.php
+++ views/main/login.php
@@ -2,10 +2,9 @@
     <form action="/enter/" method="post" class="loginForm">
         <input type="email" placeholder="Email" name="email" required>
         <input type="password" placeholder="Password" name="password" required>
+        <span><?php echo "$wrongInfo $wrongEmail $wrongPassword" ?></span>
         <input type="submit" name="login">
     </form>
-    <?php
-    echo "$wrongInfo $wrongEmail $wrongPassword"
-    ?>
+
 </main>
 
